da366cb876a5265992a99d575021375201d4ac27
Muhammad Ahmad <muhammadahmadma4642@gmail.com>
2025-05-08 18:29:25 +0500

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <ClerkProvider
-        appearance={{
+        appearance={{ 
           layout: {
             socialButtonsVariant: "iconButton",
             logoImageUrl: "/icons/yoom-logo.svg",
